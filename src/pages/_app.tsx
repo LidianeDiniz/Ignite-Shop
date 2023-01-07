798779de722a1ebaf05c62ac9486c36a176f8470
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-01-07 19:28:48 -0300

    Header/ImagesNext.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global';
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+import Image from 'next/image'
+
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+
+      <Header>
+        <Image src ={logoImg} alt=""/ >
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+
+  )
 }
