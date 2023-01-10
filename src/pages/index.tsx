367c267424740c02583acd7773c1661f3c30e8ce
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-01-09 21:30:20 -0300

    Create carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image";
 import { HomeComatainer, Product } from "../styles/pages/home";
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
+import {useKeenSlider} from 'keen-slider/react';
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides:{
+      perView: 2,
+      spacing: 48,
+    }
+  })
+
   return (
-<HomeComatainer>
-  <Product>
+<HomeComatainer ref={sliderRef} className ='keen-slider'>
+
+  <Product className="keen-slider__slide">
     <Image src={camiseta1} width={520} height={480} alt=""/>
     
         <footer>
@@ -16,13 +26,35 @@ export default function Home() {
     </footer>
   </Product>
 
-  <Product>
-  <Image src={camiseta2} width={520} height={480}  alt=""/>
+  <Product className="keen-slider__slide">
+    <Image src={camiseta2} width={520} height={480} alt=""/>
+    
+        <footer>
+      <strong>Camiseta 2</strong>
+      <span>R$ 79,90</span>
+    </footer>
+  </Product>
+
+
+  <Product className="keen-slider__slide">
+    <Image src={camiseta3} width={520} height={480} alt=""/>
+    
+        <footer>
+      <strong>Camiseta 3</strong>
+      <span>R$ 79,90</span>
+    </footer>
+  </Product>
+
+  <Product className="keen-slider__slide">
+    <Image src={camiseta1} width={520} height={480} alt=""/>
+    
         <footer>
       <strong>Camiseta 1</strong>
       <span>R$ 79,90</span>
     </footer>
   </Product>
+ 
+  
 </HomeComatainer>
   )
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,7 @@ import { styled } from "..";
 
 export const HomeComatainer = styled('main', {
 display : 'flex',
-gap: '3rem',
+//gap: '3rem',
 width: '100%',
 maxWidth:' calc(100vw - ((100vw - 1180px ) / 2))',
 marginLeft: 'auto',
@@ -13,7 +13,7 @@ minHeight: 656,
 export const Product =styled('a',{
     background: 'linear-gradient(188deg, #1ea483 0%, #7465d4 100%);',
     borderRadius: 8,
-    padding: '0.25rem',
+   // padding: '0.25rem',
     cursor: 'pointer',
     position:'relative',
     overflow: "hidden",
