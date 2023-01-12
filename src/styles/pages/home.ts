ca8ed859e4dca46c77fece6106488928f39f9eaa
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-01-12 20:38:25 -0300

    Navigation Link

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,71 +1,63 @@
 import { styled } from "..";
 
-export const HomeComatainer = styled('main', {
-display : 'flex',
-//gap: '3rem',
-width: '100%',
-maxWidth:' calc(100vw - ((100vw - 1180px ) / 2))',
-marginLeft: 'auto',
-minHeight: 656,
-
+export const HomeComatainer = styled("main", {
+  display: "flex",
+  //gap: '3rem',
+  width: "100%",
+  maxWidth: " calc(100vw - ((100vw - 1180px ) / 2))",
+  marginLeft: "auto",
+  minHeight: 656,
 });
 
-export const Product =styled('a',{
-    background: 'linear-gradient(188deg, #1ea483 0%, #7465d4 100%);',
-    borderRadius: 8,
-   // padding: '0.25rem',
-    cursor: 'pointer',
-    position:'relative',
-    overflow: "hidden",
+export const Product = styled('div', {
+  background: "linear-gradient(188deg, #1ea483 0%, #7465d4 100%);",
+  borderRadius: 8,
+  // padding: '0.25rem',
+  cursor: "pointer",
+  position: "relative",
+  overflow: "hidden",
 
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 
-    img: {
-        objectFit: 'cover',
+  img: {
+    objectFit: "cover",
+  },
+
+  footer: {
+    position: "absolute",
+    bottom: "0.25rem",
+    left: "0.25rem",
+    right: "0.25rem",
+    padding: "2rem",
+    borderRadius: 6,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    background: "rgba(0, 0, 0, 0.6)",
+
+    transform: "translateY(110%)",
+    opacity: 0,
+    transition: "all 0.3s ease-in-out",
+
+    strong: {
+      fontSize: "$lg",
+      color: "$gray100",
     },
 
+    span: {
+      fontSize: "$xl",
+      fontWeight: "bold",
+      color: "$green300",
+    },
+  },
+
+  "&:hover": {
     footer: {
-        position: 'absolute',
-        bottom: '0.25rem',
-        left: '0.25rem',
-        right: '0.25rem',
-        padding: '2rem',
-        borderRadius: 6,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-
-        background: 'rgba(0, 0, 0, 0.6)',
-        
-        transform: 'translateY(110%)',
-        opacity: 0,
-        transition: 'all 0.3s ease-in-out',
-        
-        
-               
-
-
-        strong: {
-            fontSize: '$lg',
-        },
-
-        span: {
-            fontSize: '$xl',
-            fontWeight: 'bold',
-            color: '$green300',
-        }
-
+      transform: "translateY(0%)",
+      opacity: 1,
     },
-
-    '&:hover':{
-        footer: {
-            transform: 'translateY(0%)',
-            opacity: 1,
-        }
-    }
-
-    
-
-})
+  },
+});
