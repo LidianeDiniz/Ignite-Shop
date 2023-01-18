67dbf969e2bc493f5e20cb97b22ed77267535598
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-01-18 19:41:10 -0300

    Deploy

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 
-console.log(process.env.STRIPE_SECRET_KEY)
+
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string,{
 apiVersion: '2022-11-15',
 appInfo: {
