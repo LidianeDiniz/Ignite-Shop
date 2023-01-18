40c3efe4b44ee037adf71d3fb53338d796de25bc
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-01-18 20:11:16 -0300

    Deploy

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -3,18 +3,27 @@ import { stripe } from "../../lib/stripe";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { priceId } = req.body;
-  const { host } = req.headers;
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed." });
   }
+    const { x_authorization } = req.headers
+    
+    if (x_authorization === `Bearer ${process.env.NEXT_URL}`) {
+      
+      //TODO: YOU DO YOUR THING HERE!!!
+      
+      res.status(200)
+    } else {
+      res.status(401).end()
+    }
+    
+    if (!priceId) {
+      return res.status(400).json({ error: 'Price not found.' });
+    }
 
-  if (!priceId) {
-    return res.status(400).json({ error: 'Price not found.' });
-  }
-
-  const successUrl = `https://${host}/success?session_id={CHECKOUT_SESSION_ID}`;
-  const cancelUrl = `https://${host}/`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
